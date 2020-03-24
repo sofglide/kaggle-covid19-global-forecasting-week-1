fc1e6a82767e938627646fe2875dacbd5ebde542
Sofiane Soussi <sofiane.soussi@klarna.com>
2020-03-24 02:32:14 +0100

    notebook fix for timedelta on x axis

--- covid/plotting.py
+++ covid/plotting.py
@@ -34,13 +34,21 @@ def plot_by_country(
     title = title if title else column
 
     _, ax = plt.subplots(figsize=figsize)
+    data_copy = data.copy()
+    if data_copy.index.dtype == 'timedelta64[ns]':
+        data_copy.index = (data_copy.index / pd.Timedelta(1, 'd')).astype(int)
+        data_copy.index.name = 'days'
 
-    data.groupby("country")[column].plot(
+    grouped_data = data_copy.groupby("country")[column]
+    grouped_data.plot(
         kind=kind, logy=logy, legend=True, grid=True, title=title, ax=ax, **kwargs
     )
+
     ax.set(ylabel=column)
-    # set ticks every week
-    ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-    # set major ticks format
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    if data_copy.index.dtype == 'datetime64[ns]':
+        # set ticks every week
+        ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+        # set major ticks format
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    
     plt.show()
