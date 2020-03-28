726f4b2b1e9173fb454fb9813d63f24fb10062a2
Sofiane Soussi <sofiane.soussi@klarna.com>
2020-03-28 18:17:31 +0100

    cosmetic adjustments to plots

--- covid/plotting.py
+++ covid/plotting.py
@@ -1,8 +1,11 @@
 """
 Plotting functions
 """
+import math
+
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 
 
@@ -31,6 +34,16 @@ def plot_by_country(
     figsize : tuple, optional
         pyplot figsize argument, by default (15, 7)
     """
+
+    def round_y(y_limits):
+        if logy:
+            return [
+                10 ** math.floor(math.log10(max(0.1, y_limits[0]))),
+                10 ** math.ceil(math.log10(y_limits[1])),
+            ]
+
+        return [0, math.ceil(y_limits[1])]
+
     title = title if title else column
 
     _, ax = plt.subplots(figsize=figsize)
@@ -44,11 +57,21 @@ def plot_by_country(
         kind=kind, logy=logy, legend=True, grid=True, title=title, ax=ax, **kwargs
     )
 
-    ax.set(ylabel=column)
+    x_lim = data_copy.index.min(), data_copy.index.max()
+    y_lim = data_copy[column].min(), data_copy[column].max()
+
     if data_copy.index.dtype == "datetime64[ns]":
+        ax.xaxis.set_minor_formatter(mdates.DateFormatter(""))
         # set ticks every week
-        ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+        ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.SU))
         # set major ticks format
         ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
+    else:
+        ax.set_xlim([0, math.ceil(x_lim[1])])
+        ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+
+    ax.grid("on", which="minor", linestyle="--")
+    ax.set(ylabel=column)
+    ax.set_ylim(round_y(y_lim))
 
     plt.show()
